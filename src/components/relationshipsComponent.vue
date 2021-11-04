db299cf6246a53633d5269a77d3182ca69535787
Austin Caron <austin.caron1@gmail.com>
2021-11-04 16:41:45 -0400

    added data for timeine/relationship. added styles for character-specific. Added components for character

--- src/components/relationshipsComponent.vue
+++ src/components/relationshipsComponent.vue
@@ -1,12 +1,50 @@
 <template>
-  <section>
-      <h1>Relationships</h1>
+  <section class="relationship">
+    <div class="relationship__charWrapper">
+      <span class="relationship__char" @click="displayInfo(character.id, $event)" v-for="character in currentTab" :key="character.id">{{ character.character }}</span>
+    </div>
+    <div class="relationship__data">
+      <div class="relationship__info" v-for="info in currentRelationship.content" :key="info.id">
+        <img v-if="info.img" :src="info.img" :alt="'Image of ' + currentRelationship.character + `'s relationship with ` + this.$route.params.character" class="relationship__img">
+        <p :class="{'has-img': info.img}" class="relationship__details">{{ info.details }}</p>
+      </div>
+    </div>
   </section>
 </template>
-
 <script>
 export default {
-    props: ["currentTab"]
+    props: ["currentTab"],
+
+    data() {
+      return {
+        currentRelationship: {
+          id: "",
+          content: []
+        }
+      }
+    },
+
+    mounted() {
+      console.log(this.currentTab)
+
+      this.currentRelationship = this.currentTab[0]
+      this.$el.querySelector(".relationship__char").classList.add("active")
+    },
+
+    methods: {
+      displayInfo(charId, e) {
+        this.currentRelationship = this.currentTab.find(char => char.id === charId)
+        console.log(this.currentRelationship)
+
+        const selected = e.target
+
+        const relationChars = document.querySelectorAll(".relationship__char")
+        relationChars.forEach(char => {
+          if (char === selected) { char.classList.add("active") } 
+          else { char.classList.remove("active") }
+        })
+      }
+    }
 }
 </script>
 
--- src/components/timelineComponent.vue
+++ src/components/timelineComponent.vue
@@ -1,6 +1,8 @@
 <template>
   <section class="timeline">
-    <span class="timeline__year" @click="displayInfo(year.id)" v-for="year in currentTab" :key="year.id">{{ year.year }}</span>
+    <div class="timeline__yearWrapper">
+      <span class="timeline__year" @click="displayInfo(year.id, $event)" v-for="year in currentTab" :key="year.id">{{ year.year }}</span>
+    </div>
     <div class="timeline__events">
       <div class="timeline__event" v-for="event in currentYear" :key="event.id">
         <p class="timeline__details">{{ event.details }}</p>
@@ -21,12 +23,23 @@ export default {
 
     mounted() {
       this.currentYear = this.currentTab[0].events
+
+      // add class "active" to the first year
+      this.$el.querySelector(".timeline__year").classList.add("active")
     },
 
     methods: {
-      displayInfo(yearId) {
+      displayInfo(yearId, e) {
         console.log(yearId)
         this.currentYear = this.currentTab.find(year => year.id === yearId).events
+
+        const selected = e.target
+
+        const timelineYears = document.querySelectorAll(".timeline__year")
+        timelineYears.forEach(year => {
+          if (year === selected) { year.classList.add("active") } 
+          else { year.classList.remove("active") }
+        })
       }
     }
 }
