ee9ff0ad057f48e3c0300948bd5e1bd719d031cc
Austin Caron <austin.caron1@gmail.com>
2021-11-05 20:46:51 -0400

    added key moments component, added basic styles

--- src/components/keyMomentsComponent.vue
+++ src/components/keyMomentsComponent.vue
@@ -1,12 +1,43 @@
 <template>
-  <section>
-      <h1>Key Moments</h1>
+  <section class="moments">
+    <div class="moments__data">
+      <div class="moments__info" v-for="info in currentMoments" :key="info.id">
+        <img v-if="info.img" :src="info.img" :alt="'Image of ' + info.title" class="moments__img">
+        <h2 class="moments__title">{{ info.title }}</h2>
+        <p v-for="para in info.details" :key="para.id" class="moments__details">{{ para.content }}</p>
+      </div>
+    </div>
   </section>
 </template>
-
 <script>
 export default {
-    props: ["currentTab"]
+    props: ["currentTab"],
+
+    data() {
+      return {
+        currentMoments: {}
+      }
+    },
+
+    mounted() {
+      this.currentMoments = this.currentTab
+      console.log(this.currentMoments)
+    },
+
+    methods: {
+      displayInfo(charId, e) {
+        this.currentMoments = this.currentTab.find(char => char.id === charId)
+        console.log(this.currentMoments)
+
+        const selected = e.target
+
+        const relationChars = document.querySelectorAll(".moments__char")
+        relationChars.forEach(char => {
+          if (char === selected) { char.classList.add("active") } 
+          else { char.classList.remove("active") }
+        })
+      }
+    }
 }
 </script>
 
